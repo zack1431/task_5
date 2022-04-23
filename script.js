d5a8055675429b0d2144a6b1555e548ff9a8fc58
zack1431 <zakrshkh16@gmail.com>
2022-04-23 13:40:07 +0530

    programs updated

--- script.js
+++ script.js
@@ -1,204 +1,119 @@
+//Do the below programs in anonymous function & IIFE
+//Print odd numbers in an array
 
-/*How to compare two JSON have the same properties without order?*/
-function checkObjCompare(){
-    var obj1 = { name: "Person 1", age:5 };
-    var obj2 = { age:5, name: "Person 1" };
-  
-    let c = _.isEqual(obj1, obj2) ? 'object is same' : 'object is different'
-    console.log(c)
-    
-  }
-  // checkObjCompare()
-  /*xhttp request example*/
-  /*2.Use the rest countries API url -> https://restcountries.eu/rest/v2/all and display all the country flags in console
-  3.Use the same rest countries and print all countries name, region, sub region and population
-  */
-  function getxhttprequest(){
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      let response  = JSON.parse(this.responseText);
-      response.forEach(val =>{
-          // console.log(val)
-        document.write('<img src='+val.flag+' width="50" height="50">')//not able to print in console so printed on window
-        console.log('Name: '+val.name+'\n'+'Region: '+val.region+'\n'+'Sub Region: '+val.subregion+'\n'+'Population: '+val.population)
-      })
-    }
-    xhttp.open("GET", "https://restcountries.com/v2/all");
-    xhttp.send();
-  }
-    
-  // getxhttprequest();
-  /*fetch api request example get based on id or get all data*/
-  
-  function getBasedOnId(){
-    fetch('https://jsonplaceholder.typicode.com/posts?id=10')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-  }
-  
-  
-  function checkString(str){
-    if(str.length > 6) return true;
-    else return false;
-  }
-  
-    /*delete request*/
-    function deleteapi(){
-      fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'DELETE',
-      }).then((response) => console.log(response));
-    }
-    // deleteapi();
-  
-    function getall(){
-    fetch('https://restcountries.com/v2/all',{
-      mode: 'cors',
-      headers: {
-        'Access-Control-Allow-Origin':'*'
-      }
-    })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-  }
-    // getall();
-  
-      /*update request*/
-    function updateapi(){  
-      fetch('https://jsonplaceholder.typicode.com/posts/1', {
-        method: 'PUT',
-        body: JSON.stringify({
-          id: 1,
-          title: 'foo',
-          body: 'bar',
-          userId: 1,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-    }
-    // updateapi();
-  
-    /*post api*/
-  
-  function postData(){
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: JSON.stringify({
-        title: 'foo',
-        body: 'bar',
-        userId: 1,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-  }
-  
-  let a = [1,2,3,-5,-3,-2]
-  
-  function checkNum(){
-    let b;
-    a.forEach(val =>{
-        b = isNaN(val) ? 'positive' : 'negative';//math.sign()
-        console.log(val+' '+b);
-    })
-  }
-  // checkNum();
-  
-  let b = "guvi"
-  const str = () =>{
-   return b.split("").reverse().join("")
-  }
-  // console.log(str());
-  var asp = [2,3,4,5]
-  var bsp = [4,5,6,7]
-  
-  function add(a){
-    let sums = 0;
-    a.forEach(val =>{
-      sums = sums + parseInt(val)
-    })
-    console.log(sums)
-  
-  
-  }
-  asp.push(...bsp)
-  add(asp)
-  //function with spread operator
-  // function strs(...str){
-  //   let returnstr = []
-  //   for(let a of str){
-  //       returnstr = ([...returnstr,a.length])
-  //   }
-  //   return returnstr
-  // }
-  
-  //arrow function
-  let strs = (...str) => {
-    let returnstr = 0
-    for(let a of str){
-        returnstr += parseInt(a);
-    }
-    return returnstr;
-  }
-  console.log(strs(1,2,3,4,5));
-  
-  /*template literal*/
-  let astr = 'Hello ';
-  let bstr = 'Me';
-  console.log(`${astr}${bstr}`)
-  
-  //Write a “person” class to hold all the details.
-  
-  class person {
-    
-    constructor(){
-      var self = this;
-          self.obj = {
-              name : 'Zakir Shaikh',
-              profession: 'developer',
-              id: 1,
-              btd: '31/03/1995',
-              age: 27,
-              nationality: 'India',
-              gender: 'Male',
-              hobby: 'cricket'
+  function printOdd(){
+      let arr = [1,2,3,4,5,6,7,8,9];
+      let result = [];
+      arr.forEach(val =>{
+          if(val % 2 != 0){
+              result.push(val);
           }
-    }
+      })
+      console.log(result);
   }
-  
-  let information = new person().obj
-  console.log(information);
-  
-  //write a class to calculate uber price.
-  
-  //Base Fare + (Cost per minute * time in ride) + (Cost per mile * ride distance) = Your Fare
-  
-  class uber {
-    constructor(obj){
-      var self = this;
-      self.obj = obj
-    }
-    calculate() {
-      var totalFare = 0,self=this;
-      totalFare = self.obj.baseFare + parseInt(self.obj.perMin * self.obj.rideTime) + parseInt(self.obj.perKm * self.obj.rideKm);
-      return totalFare.toFixed(2);
-    } 
+  printOdd()
+
+  //Convert all the strings to title caps in a string array
+  function converStringTitleCaps(){
+      let arr = ['jakir','alina','vahab','zarina','karina'];
+      let res = []
+      arr.forEach(val =>{
+          if(typeof(val) == 'string')
+          {
+              res.push(val.charAt(0).toUpperCase() + val.substring(1))
+          }
+      })
+      console.log(res);
   }
+  converStringTitleCaps();
+
+  //Sum of all numbers in an array
+  function sumArr(){
+      let arr = [1,2,3,4,56,7,7,8];
+      let sum = 0;
+      arr.forEach(val =>{
+        sum += val;
+      })
+      console.log(sum);
+  }
+  sumArr();
+
+  //Remove duplicates from an array
+  function removeDupArr(){
+    let chars = ['A', 'B', 'A', 'C', 'B'];
+
+    let uniqueChars = chars.filter((c, index) => {
+        return chars.indexOf(c) === index;
+    });
+    console.log(uniqueChars);
+  }
+  removeDupArr()
+
+  //Return all the prime numbers in an array
+  function primeArr(){
+      let arr = [1,2,4,5,6,7,19,23,45];
+      let i = 2;
+      let res = [];
+      arr.forEach(val =>{
+          if(val == 1){
+              console.log('1 is neither prime nor composite number')
+          }
+          else if(val > 1){
+              if(val % i != 0){
+                res.push(val);
+                
+              }
+              else{
+                  console.log(val+' is not a prime number')
+              }
+              i++;
+          }
+      })
+      console.log(res)
+  }
+  primeArr()
+
+  //Return all the palindromes in an array
+  function isPalindrome() {
+    let arr = ['noon','lol','bear','bob','javascript'];
+    let res = [];
+    arr.forEach(val =>{
+        val == val.split("").reverse().join("") ? res.push(val) : ''
+    })  
+    console.log(res);
+ }
+ isPalindrome();
+
+ //Return median of two sorted arrays of the same size
+ function median(){
+     var a = [3,56,8,9,7];
+     var b = [2,16,14,80,6];
+     var c = a.concat(b);
+    let d = c.sort(function(a, b) {
+        return a - b;
+      });
+    if(d.length % 2 == 0){
+        let m1 = d.length/2, m2 = d.length/2 + 1;
+        let median = Math.floor(d[m1] + d[m2] / 2);
+        console.log(median)
+    }
+    else
+    {
+        console.log('To get median array size should be same')
+    }
+    
+ }
+ median();
+
+
+ //rotate an array k times
+ const rotateArray1 = function(nums, k) {
+
+    for (let i = 0; i < k; i++) {
+        nums.unshift(nums.pop());
+    }
   
-  var getFare = new uber({
-    baseFare: 48,
-    perKm : 14.7,
-    perMin : 1,
-    rideKm : 10,
-    rideTime : 32 //in min
-  })
-  
-  console.log(getFare.calculate())
-  
-  
+    return nums;
+  }
+
+  console.log(rotateArray1([2,3,4,5,6,7],3))
